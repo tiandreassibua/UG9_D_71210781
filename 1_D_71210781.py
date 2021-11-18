fe5900136aa3ba3075c78b46726592cad3308714
Andreas Sibua <93329612+tiandreassibua@users.noreply.github.com>
2021-11-18 23:10:36 +0700

    Update 1_D_71210781.py

--- 1_D_71210781.py
+++ 1_D_71210781.py
@@ -1,5 +1,8 @@
 d1 = int(input("Masukan d1 :"))
 d2 = int(input("Masukan d2 :"))
-luas = 0.5*(d1*d2)
 
-print("luas layangan : ", luas,"cm")
+luas = 0.5*(d1*d2)
+print("luas layangan : "+luas+"cm")
+
+
+
